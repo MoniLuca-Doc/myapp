1b2668beec6c1399639284770ba249b6885932aa
l.moni <l.moni@aldocapitini.edu.it>
2025-03-31 14:29:58 +0000

    lezione 31/03

---
+++ lib/widgets/bar_chart.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+import 'dart:ui_web';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+class BarChart extends StatelessWidget {
+  const BarChart({required this.expenses, super.key});
+
+  final List<double> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    double mostExpensive = 0;
+    for (var price in expenses) {
+      if (price > mostExpensive) {
+        mostExpensive = price;
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          const Text(
+            'Spese Settimanali',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+            ),
+          ),
+          const SizedBox(height: 5.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_back),
+                iconSize: 30.0,
+              ),
+              const Text(
+                'Settimana corrente',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
+              ),
+
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_forward),
+                iconSize: 30.0,
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 30.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Bar(
+                label: 'Lunedì',
+                amountSpent: expenses[0],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                label: 'Martedì',
+                amountSpent: expenses[1],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                label: 'Mercoledì',
+                amountSpent: expenses[2],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                label: 'Giovedì',
+                amountSpent: expenses[3],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                label: 'Venerdì',
+                amountSpent: expenses[4],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                label: 'Sabato',
+                amountSpent: expenses[5],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                label: 'Domenica',
+                amountSpent: expenses[6],
+                mostExpensive: mostExpensive,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Bar extends StatelessWidget {
+  const Bar({
+    required this.label,
+    required this.amountSpent,
+    required this.mostExpensive,
+    super.key,
+  });
+
+  final String label;
+  final double amountSpent;
+  final double mostExpensive;
+
+  final double _maxBarHeight = 150.0;
+
+  @override
+  Widget build(BuildContext context) {
+    final barHeight = amountSpent / mostExpensive * _maxBarHeight;
+    return Column(
+      children: [
+        Text(
+          '€${amountSpent.toStringAsFixed(2)}',
+          style: const TextStyle(
+            fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 6.0,),
+        Container(
+          height: barHeight,
+          width: 18.0,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius:   BorderRadius.circular(6.0),
+          ),
+        ),
+
+
+
+        
+      ]
+
+        ),
+    ],
+    );
+}
