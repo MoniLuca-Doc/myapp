86dfbf646d25f053b4898c54ce7aabb7c88f2743
l.moni <l.moni@aldocapitini.edu.it>
2025-04-28 14:15:53 +0000

    lezione 28/04/2025

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+import 'dart:ui';
+
 import 'package:myapp/data/data.dart';
 import 'package:myapp/helpers/color_helper.dart';
 import 'package:myapp/widgets/bar_chart.dart';
@@ -9,4 +12,129 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  _buildCategory(Category category, double totalAmountSpend) {
+    return GestureDetector(
+      onTap:
+          () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => CategoryScreen(category: category),
+            ),
+          ),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        height: 100.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Text(
+                  category.name,
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '€${(category.maxAmount - totalAmountSpend).toStringAsFixed(2)} / €${category.maxAmount.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10.0),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                final double maxBarWidth = constraints.maxWidth;
+                final double percent =
+                    (category.maxAmount - totalAmountSpend) /
+                    category.maxAmount;
+
+                double barWidth = percent * maxBarWidth;
+
+                if (barWidth < 0) {
+                  barWidth = 0;
+                }
+                return Stack(
+                  children: [
+                    Container(
+                      height: 20.0,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                    Container(
+                      height: 20.0,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                        color: getColor(context, percent),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 100.0,
+            forceElevated: true,
+            pinned: true,
+            backgroundColor: Theme.of(context).primaryColor,
+            leading: IconButton(
+              icon: const Icon(Icon.settings),
+              onPressed: () {},
+              iconSize: 30.0,
+            ),
+            flexibleSpace: const FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text('Budget'),
+            ),
+           actions: <Widget> [
+            IconButton(
+              onPressed: () {}, //QUA INSERIRE COLLEGAMENTO A ADD_EXPENSE_PAGE
+              icon: const Icon(Icons.add_cirle_outline),
+            ),
+           ], 
+          ),
+
+
+
+          
+        ],
+      ),
+    );
+  }
 }
